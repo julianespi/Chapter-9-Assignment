69418df0f31c1af883ab1a921b4ad35dbb3e5cbf
WesleyGoo <99233770+WesleyGoo@users.noreply.github.com>
2022-04-22 08:23:17 -0700

    Add files via upload

--- GuessEngine.cpp
+++ GuessEngine.cpp
@@ -2,20 +2,29 @@
 #include <stdlib.h>
 #include "input.h"
 
-//precondition: none
-//postcondition: runs a number guessing game
-int guessGame(int guess, int limit, int guessCount, int answer)
+int guessGame(int guess, int limit, int guessCount, int answer, int upperLimit, int lowerLimit)
 {
 	srand(time(0));
 	char userAnswer;
-	int upperLimit = 0;
-	int lowerLimit = 1;
-	upperLimit = limit;
 	if (guess == answer)
 		return 0;
 	else
 	{
 		guessCount++;
+		if (guess == limit)
+		{
+			answer = limit;
+			cout << "Your answer must be " << answer << "." << endl;
+			cout << "Your number was guessed in " << guessCount << " guess(es)." << endl;
+			return guessGame(answer, limit, guessCount, answer, upperLimit, lowerLimit);
+		}
+		if (guess == 1)
+		{
+			answer = 1;
+			cout << "Your answer must be " << answer << "." << endl;
+			cout << "Your number was guessed in " << guessCount << " guess(es)." << endl;
+			return guessGame(answer, limit, guessCount, answer, upperLimit, lowerLimit);
+		}
 		guess = rand() % upperLimit + lowerLimit;
 		cout << "Is your number " << guess << "?" << endl;
 		userAnswer = inputChar("Please answer with a 'y' for yes and a 'n' for no: ", 'y', 'n');
@@ -24,7 +33,7 @@ int guessGame(int guess, int limit, int guessCount, int answer)
 		{
 			answer = guess;
 			cout << "Your number was guessed in " << guessCount << " guess(es)." << endl;
-			return guessGame(answer, limit, guessCount, answer);
+			return guessGame(answer, limit, guessCount, answer, upperLimit, lowerLimit);
 		}
 		if (userAnswer == 'n')
 		{
@@ -33,38 +42,18 @@ int guessGame(int guess, int limit, int guessCount, int answer)
 			cout << endl;
 			if (userAnswer == 'y')
 			{
-				if (guess == limit)
-				{
-					answer = limit;
-					cout << "Your answer must be " << answer << "." << endl;
-					return guessGame(answer, limit, guessCount, answer);
-				}
-				else
-				{
 					lowerLimit = guess + 1;
-					return guessGame(guess, limit, guessCount, answer);
-				}
+					return guessGame(guess, limit, guessCount, answer, upperLimit, lowerLimit);
 			}
 			if (userAnswer == 'n')
 			{
-				if (guess == 1)
-				{
-					answer = 1;
-					cout << "Your answer must be " << answer << "." << endl;
-					return guessGame(answer, limit, guessCount, answer);
-				}
-				else
-				{
 					upperLimit = guess - 1;
-					return guessGame(guess, limit, guessCount, answer);
-				}
+					return guessGame(guess, limit, guessCount, answer, upperLimit, lowerLimit);
 			}
 		}
 	}	
 }
 
-//precondition: none
-//postcondition: sets up a number guessing game
 void guess()
 {
 	int guessLimit = 0;
@@ -72,5 +61,5 @@ void guess()
 	cout << "Welcome to the number guesser." << endl << "This program will guess a number from 1 to " << guessLimit << "." << endl;
 	cout << "Think of a number from 1 to " << guessLimit << "." << endl << "Press any key to begin." << endl;
 	cin.ignore();
-	guessGame(1000, guessLimit, 0, 0);
+	guessGame(1000, guessLimit, 0, 0, guessLimit, 1);
 }
